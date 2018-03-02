3cde2ace19b24f3d777e77f3e9f5fd0f9ad1930b
PJ Frias <peejfrias@gmail.com>
2018-03-02 19:02:17 +0000

    save

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -3,4 +3,23 @@ class StaticController < ApplicationController
   def index
   end
 
+  def signup
+    @user = User.new
+  end
+
+  def signin
+    @user = User.new
+  end
+
+  def login
+    @user = User.find(params[:user][:name])
+    if @user && @user.password == params[:user][:password]
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      redirect_to '/'
+    end
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get '/', to: 'static#index'
   get '/signin', to: 'static#signin'
+  post '/login', to: 'static#login'
 
   resources :users
   resources :attractions
