0421dada81546cf259b981981830271a193bda70
PJ Frias <peejfrias@gmail.com>
2018-03-04 21:38:06 +0000

    automatically backed up by learn

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -3,7 +3,7 @@ class RidesController < ApplicationController
   def create
     ride = Ride.create(:user_id => current_user.id, :attraction_id => params[:ride][:attraction_id])
     ride.take_ride
-    redirect_to user_path(current_user)
+    render "/users/show"
   end
 
 end
