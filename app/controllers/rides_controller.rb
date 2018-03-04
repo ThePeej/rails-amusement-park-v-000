3aeb5ba787f0b4ae581435f3541714dea8c757ba
PJ Frias <peejfrias@gmail.com>
2018-03-04 21:40:13 +0000

    automatically backed up by learn

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -3,6 +3,7 @@ class RidesController < ApplicationController
   def create
     ride = Ride.create(:user_id => current_user.id, :attraction_id => params[:ride][:attraction_id])
     ride.take_ride
+    @message = "Thanks for riding the #{Attraction.find(ride.attraction_id)}"
     render "/users/show"
   end
 
